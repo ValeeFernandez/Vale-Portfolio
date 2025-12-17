2861d923a81b16bd56280ed89d1cc650665e23e1
Valeria <131839057+ValeeFernandez@users.noreply.github.com>
2025-12-16 22:33:03 -0600

    Basic structure and navigation components

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#eceae6ff",
+        primary: "#7D6AA6",
+        primaryDark: "#6F618C",
+        accent: "#D989CB",
+      },
+    },
+  },
+  plugins: [],
+};
+
+
+
+
+
+
+
+
